f2976db6d7b334c0cddc72f94b3bde4a83a3bbdb
cha-do <68767765+cha-do@users.noreply.github.com>
2024-01-25 02:46:49 -0500

    Updated landScapeSOM.py

--- landScapeSOM.py
+++ landScapeSOM.py
@@ -12,9 +12,20 @@ from os import path
 import multiprocessing as mp
 from functools import partial
 
-def use_SOM(works, k, paramsInterest=None, log = True):
+def use_SOM(k, works, paramsInterest=None, log = True):
     profile = works[k]["profile"]
     SOMparameters = works[k]["SOMparameters"]
+    print(f'Work {k}: {nameSOM}')
+    # %%
+    top = SOMparameters[0]
+    X = SOMparameters[1]
+    Y = X
+    lr = SOMparameters[2]
+    sg = SOMparameters[3]
+    epochs = SOMparameters[4]
+    pca = SOMparameters[5]
+    seed = SOMparameters[6]
+    nameSOM = f'{top[:3]}_{X}x{Y}_lr{lr}_sg{sg}_eps{int(epochs/100)}E2_pca{pca}_s{seed}'
     # Read the CSV file into a DataFrame
     alldata = pd.read_csv(f"./Datasets/{profile}__AGA.csv")
     # Display the DataFrame
@@ -31,16 +42,6 @@ def use_SOM(works, k, paramsInterest=None, log = True):
         normaldataunique[column] = (dataunique[column] - dataunique[column].min()) / (dataunique[column].max() - dataunique[column].min())
     # Display the normalized DataFrame
     # print(normaldataunique)
-    # %%
-    top = SOMparameters[0]
-    X = SOMparameters[1]
-    Y = X
-    lr = SOMparameters[2]
-    sg = SOMparameters[3]
-    epochs = SOMparameters[4]
-    pca = SOMparameters[5]
-    seed = SOMparameters[6]
-    nameSOM = f'{top[:3]}_{X}x{Y}_lr{lr}_sg{sg}_eps{int(epochs/100)}E2_pca{pca}_s{seed}'
     # %% Load neurons
     if path.isfile(f"{profile}/neurons_{nameSOM}.csv"):
         alldata = pd.read_csv(f"{profile}/neurons_{nameSOM}.csv")
@@ -99,7 +100,8 @@ def use_SOM(works, k, paramsInterest=None, log = True):
             if paramInterest in paramsname:
                 metrics = ["avg", "min", "max"]
                 for metric in metrics:
-                    paramvalue = np.ones((X,Y))*-1
+                    paramvalue = np.zeros((X,Y))
+                    paramvalue[:,:] = None
                     for i in range(X):
                         for j in range(Y):
                             indexes = alldata[(alldata['Neuron_X']==i) & (alldata['Neuron_Y']==j)].index.tolist()
@@ -112,8 +114,6 @@ def use_SOM(works, k, paramsInterest=None, log = True):
                                     paramvalue[i,j] = alldata.loc[indexes][paramInterest].max()
                     #normSSE = (sseavg-sseavg[sseavg!=-1].min())/(sseavg.max()-sseavg[sseavg!=-1].min())
                     # logSSEavg = np.log(sseavg)
-                    if log:
-                        logparam = np.log(paramvalue[paramvalue!=-1])
                     # %%
                     # Visualice the SSEmap
                     f = plt.figure(figsize=(10 ,10))
@@ -125,15 +125,16 @@ def use_SOM(works, k, paramsInterest=None, log = True):
                         plt.plot(0, 0)
                         plt.plot(X-1, (Y-1)*np.sqrt(3)/2)
                         if log:
+                            logparam = np.log10(paramvalue[np.isnan(paramvalue) != True])
                             minval = logparam.min()
                             deltaval = logparam.max()-minval
                         else:
-                            minval = paramvalue[paramvalue!=-1].min()
-                            deltaval = paramvalue.max()-minval
+                            minval = paramvalue[np.isnan(paramvalue) != True].min()
+                            deltaval = paramvalue[np.isnan(paramvalue) != True].max()-minval
                         for i in range(X):
                             for j in range(Y):
-                                if paramvalue[i, j] != -1:
-                                    color = cm.Blues((np.log(paramvalue[i, j])-minval)/deltaval)
+                                if not np.isnan(paramvalue[i, j]):
+                                    color = cm.Blues((np.log10(paramvalue[i, j])-minval)/deltaval)
                                 else:
                                     color = "k"
                                 wy = yy[(i, j)] * np.sqrt(3) / 2
@@ -156,28 +157,30 @@ def use_SOM(works, k, paramsInterest=None, log = True):
                         cb1.ax.get_yaxis().labelpad = 100
                         plt.gcf().add_axes(ax_cb)
                     else:#top == "rec"
+                        plt.pcolor(np.zeros((X,Y)).T)
                         if log:
-                            plt.pcolor(logparam, cmap=cm.Blues)  # Plot the distance_map
+                            plt.pcolor(np.log10(paramvalue).T, cmap=cm.Blues)  # Plot the distance_map
                         else:
-                            plt.pcolor(paramvalue[paramvalue!=-1], cmap=cm.Blues)  # Plot the distance_map
-                        bestperseed = pd.read_csv(f"./Datasets/bestperseed_{profile}__AGA.csv")
-                        normalbestperseed = pd.DataFrame()
-                        colors = ['red', 'green', 'blue', 'orange', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan',
-                            'magenta', 'lime', 'teal', 'indigo', 'salmon', 'darkgreen', 'skyblue', 'gold', 'orchid',
-                            'sienna', 'lightpink', 'darkgray', 'darkcyan', 'navajowhite', 'rosybrown', 'palevioletred',
-                            'mediumseagreen', 'mediumslateblue', 'cadetblue', 'darkgoldenrod', 'thistle', 'dodgerblue', 'khaki',
-                            'yellow', 'turquoise']
-                        for i in range(7):
-                            column = paramsname[i+1]
-                            normalbestperseed[column] = (bestperseed[column] - dataunique[column].min()) / (dataunique[column].max() - dataunique[column].min())
-                        best_array = normalbestperseed.values
-                        for i in range(len(best_array)):
-                            wx, wy = som.winner(best_array[i])
-                            plt.plot(wx+np.random.random(), wy+np.random.random(), ".", markerfacecolor='None',
-                            markeredgecolor=colors[i%len(colors)], 
-                            markersize=2, 
-                            markeredgewidth=0)
+                            plt.pcolor(paramvalue.T, cmap=cm.Blues)  # Plot the distance_map
                         plt.colorbar()
+                        if paramInterest == "SSE":
+                            bestperseed = pd.read_csv(f"./Datasets/bestperseed_{profile}__AGA.csv")
+                            normalbestperseed = pd.DataFrame()
+                            colors = ['red', 'green', 'blue', 'orange', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan',
+                                'magenta', 'lime', 'teal', 'indigo', 'salmon', 'darkgreen', 'skyblue', 'gold', 'orchid',
+                                'sienna', 'lightpink', 'darkgray', 'darkcyan', 'navajowhite', 'rosybrown', 'palevioletred',
+                                'mediumseagreen', 'mediumslateblue', 'cadetblue', 'darkgoldenrod', 'thistle', 'dodgerblue', 'khaki',
+                                'yellow', 'turquoise']
+                            for i in range(7):
+                                column = paramsname[i+1]
+                                normalbestperseed[column] = (bestperseed[column] - dataunique[column].min()) / (dataunique[column].max() - dataunique[column].min())
+                            best_array = normalbestperseed.values
+                            for i in range(len(best_array)):
+                                wx, wy = som.winner(best_array[i])
+                                plt.plot(wx+np.random.random(), wy+np.random.random(), ".", markerfacecolor=colors[i%len(colors)],
+                                markeredgecolor=colors[i%len(colors)], 
+                                markersize=4, 
+                                markeredgewidth=0)
                     plt.suptitle(f"{paramInterest}_{metric}_{nameSOM}")
                     plt.savefig(f"./{profile}/{nameSOM}.png",dpi=169,bbox_inches='tight')
             else:
@@ -201,6 +204,7 @@ if __name__ == "__main__":
         ]
     #paramsname = ["R_core", "t_Ain", "t_B", "t_Aout", "sAin", "sigmaR", "-log(bg)", "SSE"]
     paramsInterest = ["SSE"]
+    use_mp = True 
     log = True
     n_cores = 6
     # %% set works
@@ -214,12 +218,16 @@ if __name__ == "__main__":
     print("Total Works:", w)
     firstwork = 0
     ws = range(firstwork,w)#[0,1,2,3,4,5]
-    pool = mp.Pool(n_cores)
-    partial_work = partial(use_SOM,
-                        works = works,
-                        paramsInterest = paramsInterest,
-                        log = log
-                        )
-    pool.map(partial_work,[k for k in ws])
-    pool.close()
-    pool.join
+    if use_mp:
+        pool = mp.Pool(n_cores)
+        partial_work = partial(use_SOM,
+                            works = works,
+                            paramsInterest = paramsInterest,
+                            log = log
+                            )
+        pool.map(partial_work,[k for k in ws])
+        pool.close()
+        pool.join
+    else: 
+        for k in ws:
+            use_SOM(k = k ,works = works, paramsInterest = paramsInterest, log = log)
